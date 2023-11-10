024d95f6450d8a15a16984255319a536e2908cf0
Hanzala Yousuf <hanzalayousuf159@gmail.com>
2023-11-10 16:45:42 +0500

    add: first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts'
+import Subscribe from './Subscribe'
+import Services from './Services'
+import Footer from './Footer'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import SpecialOffer from './SpecialOffer'
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    Services,
+    Footer,
+    SuperQuality,
+    SpecialOffer,
+    CustomerReviews
+};
